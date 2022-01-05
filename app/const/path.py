1c914292a28b36f9f3a83737f3c8ff7cf362fa63
goflutterjava <goflutterjava@gmail.com>
2022-01-05 14:41:34 +0800

    rename the start script

--- app/const/path.py
+++ app/const/path.py
@@ -6,5 +6,5 @@ class PathConst:
     mate_path = home_path + "/mate"
     scripts_path = mate_path + "/scripts"
     grafana_init_script = scripts_path + "/init-grafana.sh"
-    grafana_start_script = scripts_path + "/start-grafana.sh"
+    grafana_start_script = scripts_path + "/start-daemon.sh"
     grafana_dashboard_dir = mate_path + "/dashboard"
