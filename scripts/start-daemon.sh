418bc37e15beda159351a91429113890fe05ee11
lovehzj <1922919664@qq.com>
2022-01-13 23:02:04 +0800

    use mkdir -p instead of mkdir

--- scripts/start-daemon.sh
+++ scripts/start-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $GRAFANA_HOME/mate
-mkdir $GRAFANA_HOME/logs
+mkdir -p $GRAFANA_HOME/logs
 nohup python3 $GRAFANA_HOME/mate/app/main.py >>$GRAFANA_HOME/logs/grafana_mate.stdout.log 2>>$GRAFANA_HOME/logs/grafana_mate.stderr.log &
