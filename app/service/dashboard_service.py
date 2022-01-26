2af74844081754877253545958aae1e7b42442a0
goflutterjava <95280282+goflutterjava@users.noreply.github.com>
2022-01-26 10:51:08 +0800

    Change go process datasource to Prometheus (#1)

--- app/service/dashboard_service.py
+++ app/service/dashboard_service.py
@@ -1,3 +1,22 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
 import json
 from urllib import request
 
@@ -51,8 +70,8 @@ class DashboardService:
         return json_data['id']
 
     @staticmethod
-    def import_dashboard(title, folder_id, dir, dashboard):
-        with open(PathConst.grafana_dashboard_dir + "/" + dir + "/" + dashboard) as file:
+    def import_dashboard(title, folder_id, directory, dashboard):
+        with open(PathConst.grafana_dashboard_dir + "/" + directory + "/" + dashboard) as file:
             data = file.read()
         dumps = json.dumps({"uid": title, "folderId": folder_id, "dashboard": json.loads(data)})
         body = dumps.encode(encoding='utf-8')
