eb43aef3afa3f962c949730d4d9986a47cf4f9ba
goflutterjava <goflutterjava@gmail.com>
2022-06-21 17:09:46 +0800

    add prometheus jvm actuator dashboard

--- app/service/dashboard_service.py
+++ app/service/dashboard_service.py
@@ -44,6 +44,7 @@ class DashboardService:
         self.import_dashboard("Prometheus", folder_id, "prometheus", "pulsar-jvm-by-prometheus.json")
         self.import_dashboard("Prometheus", folder_id, "prometheus", "mysql_rev1.json")
         self.import_dashboard("Prometheus", folder_id, "prometheus", "coredns_rev2.json")
+        self.import_dashboard("Prometheus", folder_id, "prometheus", "jvm-actuator_rev1.json")
         self.import_dashboard("Prometheus", folder_id, "prometheus", "jvm-micrometer_rev9.json")
         self.import_dashboard("Prometheus", folder_id, "prometheus", "go-processes_rev2.json")
         self.import_dashboard("Prometheus", folder_id, "prometheus", "pulsar-functions-by-prometheus.json")
